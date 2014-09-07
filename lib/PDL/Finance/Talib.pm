44d14ebdfb004efbf819c1edb6dd2e4c601c2437
kmx <kmx@cpan.org>
2014-09-07 21:21:07 +0000

    Import of KMX/PDL-Finance-Talib-0.008 from CPAN.
    gitpan-cpan-distribution: PDL-Finance-Talib
    gitpan-cpan-version:      0.008
    gitpan-cpan-path:         KMX/PDL-Finance-Talib-0.008.tar.gz
    gitpan-cpan-author:       KMX
    gitpan-cpan-maturity:     released

---
+++ lib/PDL/Finance/Talib.pm
@@ -0,0 +1,22 @@
+use strict;
+
+use strict;
+use warnings;
+
+our $VERSION = "0.008";
+
+sub import {
+  eval 'use PDL::Finance::TA;'
+}
+
+1;
+
+=head1 NAME
+
+PDL::Finance::Talib - DEPRECATED (use PDL::Finance::TA instead)
+
+=head1 SEE ALSO
+
+L<PDL::Finance::TA>
+
+=cut
